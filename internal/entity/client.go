621dac90d68e3aa06db96fd9fcb8384717d3ac52
Wagner <wagnerpalmeira.ti@gmail.com>
2024-10-06 16:42:16 -0300

    Updating client func

--- internal/entity/client.go
+++ internal/entity/client.go
@@ -40,3 +40,14 @@ func (c *Client) Validate() error {
 	}
 	return nil
 }
+
+func (c *Client) Update(name string, email string) error {
+	c.Name = name
+	c.Email = email
+	c.UpdatedAt = time.Now()
+	err := c.Validate()
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -19,3 +19,17 @@ func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, client)
 }
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("Jhon Doe", "a@a.com")
+	err := client.Update("Jane Doe", "j@j.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "Jane Doe", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("Jhon Doe", "a@a.com")
+	err := client.Update("", "j@j.com")
+	assert.Error(t, err, "name is required")
+}
