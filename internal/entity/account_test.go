9584255bf840373dafaed9668e9e54bda12fcd47
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2024-10-20 22:31:42 -0300

    Adding account's client

---
+++ internal/entity/account_test.go
@@ -0,0 +1,27 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAccount(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j")
+	account := NewAccount(client)
+	assert.NotNil(t, account)
+	assert.Equal(t, client.ID, account.Client.ID)
+}
+
+func TestCreateAccountWithBlankNilClient(t *testing.T) {
+	account := NewAccount(nil)
+	assert.Nil(t, account)
+}
+
+func TestCreditAccount(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j")
+	account := NewAccount(client)
+	account.Credit(100)
+	account.Debit(50)
+	assert.Equal(t, float64(50), account.Balance)
+}
